27271f6fd7ec676e084b51fa21d9d2217ee7791f
huang714669 <781797070@qq.com>
2018-08-07 23:45:32 +0800

    Update json-and-path.py

--- json-and-path.py
+++ json-and-path.py
@@ -2,16 +2,18 @@ import json
 import os.path
 
 def preAction():
-    fileDir = os.path.dirname(os.path.realpath('__file__'))
-    filename = os.path.join(fileDir, 'template','parameters.json')
-    if os.path.exists(filename):
-         with open(filename,'r' ) as f:
+#     fileDir = os.path.dirname(os.path.realpath('__file__'))
+#     filename = os.path.join(fileDir, 'template','parameters.json')//this will work on windows
+    parameters_path = os.path.join(os.path.dirname(__file__), 'template', 'parameters.json') #work on linux
+
+    if os.path.exists(parameters_path):
+         with open(parameters_path,'r' ) as f:
                 json_data=json.loads(f.read())
                 json_data['parameters']['virtualMachines_image_id']['value'] = "sa"
                 json_data['parameters']['deployer_tag']['value'] = "2434"
                 print(json_data)
                 f.close()
-         with open(filename,'w' ) as f:
+         with open(parameters_path,'w' ) as f:
                 json.dump(json_data,f)
                 f.close
                 
